21883697f754a18fc87cc564afd83642b78d3d42
kristopherfanaa <kristopher.fana@matogen.com>
2024-04-10 09:10:02 +0200

    Revert "Implemented Translations"
    This reverts commit dd371f269b7dbe5800f64eee0a8f47726a0ae269.

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
 import { resolve } from 'path';
+import { defineConfig } from 'vite'
 
 const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 export default defineConfig({
   root,
   build: {
-    assetsInlineLimit: 9000,
     outDir,
     emptyOutDir: true,
     rollupOptions: {
